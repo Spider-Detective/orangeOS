127e02ab121ac8d8fba31aafb603f9d630e765ed
ZixiXu <zixixu2017@gmail.com>
2019-11-13 16:14:13 -0800

    added Makefile and .gitignore

--- ch3/copyToFloppy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# $0 the script name, $1: .com system file, $2: floppy name
-COM_NAME="$1"
-FLOPPY_NAME="$2"
-echo "Start to copy $1 to $2"
-sudo mount -o loop $2 /mnt
-sudo cp $1 /mnt
-sudo umount /mnt
